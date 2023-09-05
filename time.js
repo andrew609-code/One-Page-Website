b8ceadec93442728ae87fb0fcd4cf38f6aa5b5ce
andrew609-code <142052575+andrew609-code@users.noreply.github.com>
2023-09-05 09:59:49 -0700

    changes made
    Used CSS to style the html site I made

--- time.js
+++
@@ -1,6 +0,0 @@
-window.onload = displayClock();
-function displayClock(){
-  var display = new Date().toLocaleTimeString();
-  document.body.innerHTML = display;
-  setTimeout(displayClock, 1000); 
-}
